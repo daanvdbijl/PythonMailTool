e6a9622c6cf9eb3dcc82467f097104d5634c4e9a
Daan <s1112309@student.hsleiden.nl>
2022-11-28 14:40:31 +0100

    "First commit, basic search and count"

---
+++ mailcleaner.py
@@ -0,0 +1,38 @@
+import imaplib
+import pandas as pd
+
+def get_creds():
+    
+    creds = pd.read_json("creds.json")
+    return creds
+
+def login(creds):
+    imap  = imaplib.IMAP4_SSL("imap.gmail.com")
+    imap.login(creds["user1"]["user"], creds["user1"]["16pass"])
+    return imap
+
+def filter_inbox(imap, sender):
+    imap.select("Inbox")
+    status, messages = imap.search(None, "X-GM-RAW", "from:%s" % sender)
+    n = len(str(messages).split(" "))
+    print("We found %s mails from %s.\n" % (n, sender))
+    imap.close()    
+
+def main():
+    creds = get_creds()
+    mImap = login(creds)
+    
+    running = True
+    while running:
+        mode = input("\nWhat would you like to do?: \n1. Select all mail from a sender.\n2. Quit.\n> ")
+        if str(mode) == "1":
+            sender = input("\nFrom what sender would you like to count email?\n> ")
+            filter_inbox(mImap, sender)
+        elif str(mode) == "2":
+            running = False
+        else:
+            print("That is not a valid option...")
+    
+    mImap.logout()
+       
+main()
